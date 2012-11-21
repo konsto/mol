dbc0889aa9e4eedc64b611a81489602de8cb688f
michalewandowski <michalewandowski@gmail.com>
2012-11-21 15:43:57 +0000

    Ressurect: IfNode.java, InvocationNode.java. Both from revision 12

---
+++ src/ast/InvocationNode.java
@@ -0,0 +1,34 @@
+package ast;
+
+import java.util.List;
+
+public class InvocationNode implements IExpressionNode {
+
+    String pluginAlias;
+    String method;
+    List<IExpressionNode> params;
+
+    public InvocationNode(String pluginAlias, String method,
+            List<IExpressionNode> params) {
+        this.params = params;
+        this.method = method;
+        this.pluginAlias = pluginAlias;
+    }
+
+    public String getPluginAlias() {
+        return this.pluginAlias;
+    }
+
+    public String getMethod() {
+        return this.method;
+    }
+
+    public List<IExpressionNode> getParams() {
+        return this.params;
+    }
+
+    @Override
+    public void accept(IVisitor visitor) {
+        visitor.visit(this);
+    }
+}

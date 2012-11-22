2bee7305fa96c56a0a4f87ad7d1fcf06ba3eea21
michalewandowski <michalewandowski@gmail.com>
2012-11-22 22:36:41 +0000

    Replaced: Old unary/binary operation mechanism with new one, using polymoprhism

---
+++ src/ast/LessThanOrEqualToExecutor.java
@@ -0,0 +1,18 @@
+package ast;
+
+import exceptions.BadOperandsForOperationException;
+
+public class LessThanOrEqualToExecutor extends AbstractBinaryOperationExecutor {
+
+    @Override
+    public Object execute(Object left, Object right) throws Exception {
+        if (areNumbers(left, right)) {
+            double temp1 = Double.parseDouble(left.toString());
+            double temp2 = Double.parseDouble(right.toString());
+            return temp1 <= temp2;
+        } else {
+            throw new BadOperandsForOperationException();
+        }
+    }
+
+}

394e813afb9984c3504e0bbdf06a7f6e363de552
michalewandowski <michalewandowski@gmail.com>
2012-11-27 03:09:32 +0000

    Added: XML Script Parser (not finished yet)

---
+++ src/xmlparser/Parser.java
@@ -0,0 +1,143 @@
+package xmlparser;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.TreeWalker;
+
+import ast.AssigmentNode;
+import ast.GroupNode;
+import ast.IExpressionNode;
+import ast.INode;
+import ast.IfNode;
+import ast.ImportNode;
+import ast.InvocationNode;
+import ast.LiteralNode;
+import ast.VariableNode;
+
+public class Parser {
+    DocumentBuilderFactory builder;
+    GroupNode root = null;
+    TreeWalker walker;
+    Document document;
+    DocumentTraversal traversal;
+    MyNodeFilter filter;
+    
+    public Parser() throws ParserConfigurationException {
+        builder = DocumentBuilderFactory.newInstance();
+    }
+
+    public void setUp(String filepath) throws Exception {
+        document = builder.newDocumentBuilder().parse(filepath);
+        traversal = (DocumentTraversal) document;
+        filter = new MyNodeFilter();
+        walker = traversal.createTreeWalker(document.getDocumentElement(),
+                NodeFilter.SHOW_ELEMENT, filter, false);
+    }
+
+    public void printTree(String indent) {
+        Node parent = walker.getCurrentNode();
+        System.out.println(indent + "- " + parent.getNodeName());
+        for (Node n = walker.firstChild(); n != null; n = walker.nextSibling()) {
+            printTree(indent + "    ");
+        }
+        walker.setCurrentNode(parent);
+    }
+
+    public GroupNode getRoot() {
+        return root;
+    }
+
+    public INode parseProgram() {
+        root = new GroupNode();
+
+        Node programNode = walker.getCurrentNode();
+        for (Node n = walker.firstChild(); n != null; n = walker.nextSibling()) {
+            INode child = parseNode(n);
+            root.addChild(child);
+        }
+        return root;
+    }
+
+    public INode parseNode(Node node) {
+        INode result = null;
+        NamedNodeMap attributes = node.getAttributes();
+        String type = node.getNodeName();
+        switch (type) {
+        case "import":
+            // System.out.println(attributes.getNamedItem("class").getNodeValue());
+            result = new ImportNode(attributes.getNamedItem("class")
+                    .getNodeValue(), attributes.getNamedItem("alias")
+                    .getNodeValue());
+            break;
+        case "int":
+            result = new LiteralNode(Integer.parseInt(attributes.getNamedItem(
+                    "value").getNodeValue()));
+            break;
+        case "double":
+            result = new LiteralNode(Double.parseDouble(attributes
+                    .getNamedItem("value").getNodeValue()));
+            break;
+        case "string":
+            result = new LiteralNode(attributes.getNamedItem("value")
+                    .getNodeValue());
+            break;
+        case "var":
+            result = new VariableNode(attributes.getNamedItem("identifier")
+                    .getNodeValue());
+            break;
+        case "assigment":
+            result = new AssigmentNode(attributes.getNamedItem("identifier")
+                    .getNodeValue(),
+                    (IExpressionNode) parseNode(walker.firstChild()));
+            break;
+        case "call":
+            String method = attributes.getNamedItem("method").getNodeValue();
+            IExpressionNode target = (IExpressionNode) parseNode(walker
+                    .firstChild());
+            List<IExpressionNode> params = new LinkedList<IExpressionNode>();
+            for (Node n = walker.nextSibling(); n != null; n = walker
+                    .nextSibling()) {
+                params.add((IExpressionNode) parseNode(n));
+            }
+            result = new InvocationNode(target, method, params);
+            break;
+        case "if":
+            TreeWalker walker2 = walker = traversal.createTreeWalker(document.getDocumentElement(),
+                    NodeFilter.SHOW_ELEMENT, filter, false);
+            Map<IExpressionNode, GroupNode> ifs = new LinkedHashMap<IExpressionNode, GroupNode>();
+            GroupNode elseExpression = null;
+            for (Node n = walker.firstChild(); n != null; n = walker.nextSibling()) {
+                if (n.getNodeName().equals("branch")) {
+                    walker2.setCurrentNode(n);
+                    //jestem na branchu
+                    for (Node m = walker2.firstChild(); m != null; m = walker2.nextSibling()) {
+                        if (m.getNodeName().equals("condition")) {
+                            //TODO: dokonczyc IFFA!!!
+                        } else if (m.getNodeName().equals("action")) {
+                            
+                        }
+                    }
+                } else if (n.getNodeName().equals("else")) {
+                    
+                }
+            }
+            result = new IfNode(ifs, elseExpression);
+            break;
+        }
+        walker.setCurrentNode(node);
+        return result;
+    }
+}
